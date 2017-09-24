d6d133fa233d337c7905a84be9180837af28510b
ZH11 <Pablo.Garcia@uk.tesco.com>
2017-09-24 23:41:32 +0200

    refactor bash query to code

--- src/politrons/Server.go
+++ src/politrons/Server.go
@@ -7,24 +7,44 @@ import (
 	"os"
 )
 
+type VisitorAction interface {
+	renderProfile(w http.ResponseWriter)
+}
+
+type Person struct {
+	profileId string
+}
+
+// Go use composition instead of inheritance, now VisitorProfile just contains profileId
+type VisitorProfile struct {
+	Person
+}
+
 func handler(w http.ResponseWriter, request *http.Request) {
 	queryValues := request.URL.Query()
 	username := queryValues.Get("username")
 	password := queryValues.Get("password")
 	visitors := getVisitors(username, password)
-	for visitor, _ := range visitors {
-		createVisitorPage(visitor)
-		visitor_page := visitor + ".html"
-		data, err := ioutil.ReadFile(visitor_page)
-		checkError(err)
-		w.Write(data)
-		time.Sleep(1 * time.Second)
-		os.Remove(visitor_page)
+	for profileId, _ := range visitors {
+		visitor := VisitorProfile{Person{profileId}}
+		actions := [...]VisitorAction{visitor}
+		actions[0].renderProfile(w)
 	}
 	data, err := ioutil.ReadFile("end.html")
 	checkError(err)
 	w.Write(data)
-	//fmt.Fprintf(w, "Hi there, I love %s!", string(data))
+}
+
+// We implement a interface method just defining the method with same name and arguments
+// and passing the class that implement as first argument
+func (visitor VisitorProfile) renderProfile(w http.ResponseWriter) {
+	createVisitorPage(visitor.profileId)
+	visitor_page := visitor.profileId + ".html"
+	data, err := ioutil.ReadFile(visitor_page)
+	checkError(err)
+	w.Write(data)
+	time.Sleep(1 * time.Second)
+	os.Remove(visitor_page)
 }
 
 func main() {
